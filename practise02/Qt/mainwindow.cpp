1294a644c4ed90d63e230c2302a3d7f57dbb6c9b
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-07-15 14:24:59 +0300

    Update mainwindow.cpp

--- practise02/Qt/mainwindow.cpp
+++ practise02/Qt/mainwindow.cpp
@@ -33,6 +33,9 @@ MainWindow::MainWindow(QWidget *parent)
     // Когда мы выбираем в меню опцию "Quit", то приложение сразу же завершает свое выполнение
     connect(quit, &QAction::triggered, qApp, QApplication::quit);
     connect(status, &QAction::triggered, qApp, QApplication::quit); //Вместо quit будет вывод окна со статистикой
+
+    timer = new QTimer; //Создаем объект таймера
+
 }
 
 MainWindow::~MainWindow()
@@ -71,39 +74,27 @@ void MainWindow::on_button_on_wifi_clicked()
     ui->label_wifi->setText("Wi-Fi модуль активирован");
 }
 
-void MainWindow::on_button_off_wifi_clicked()
-{
-    ui->lamp_wifi->setBackgroundBrush(Qt::red);
-    serialuse->writePort(QByteArray().append("wifioff"));
-    ui->label_wifi->setText("Wi-Fi модуль деактивирован");
-}
-
 void MainWindow::on_button_on_window_clicked()
 {
-    ui->lamp_window->setBackgroundBrush(Qt::green);
-    serialuse->writePort(QByteArray().append("winon"));
-    ui->label_window->setText("Окно открыто");
+    serialuse->writePort(QByteArray().append("openwin"));
 }
 
 void MainWindow::on_button_off_window_clicked()
 {
-    ui->lamp_window->setBackgroundBrush(Qt::red);
-    serialuse->writePort(QByteArray().append("winoff"));
-    ui->label_window->setText("Окно закрыто");
+
+    serialuse->writePort(QByteArray().append("closewin"));
+
 }
 
 void MainWindow::on_button_on_pump_clicked()
 {
-    ui->lamp_pump->setBackgroundBrush(Qt::green);
-    serialuse->writePort(QByteArray().append("pumpon"));
-    ui->label_pump->setText("Насос активирован");
+
+    serialuse->writePort(QByteArray().append("poweronpump"));
 }
 
 void MainWindow::on_button_off_pump_clicked()
 {
-    ui->lamp_pump->setBackgroundBrush(Qt::red);
-    serialuse->writePort(QByteArray().append("pumpoff"));
-    ui->label_pump->setText("Насос деактивирован");
+    serialuse->writePort(QByteArray().append("poweroffpump"));
 }
 
 void MainWindow::GetSerial(Serialuse *obj)
@@ -115,8 +106,9 @@ void MainWindow::GetData() //Запрос на получение данных
 {
     if (serialuse->connected)
     {
-        serialuse->writePort(QByteArray().append("getdata"));
+        serialuse->writePort(QByteArray().append("getpar")); //здесь поменял
     }
+    qDebug()<<"Что то отправили";
 }
 
 void MainWindow::datain(QByteArray indata) //Обработка полученных с контроллера данных
@@ -126,103 +118,172 @@ void MainWindow::datain(QByteArray indata) //Обработка полученн
     controllerData += DataString;
     int val = 0;
     bool ok;
-    if (indata.indexOf("paramds") != -1) //Признак начала передачи
+     qDebug()<<"Что то приняли";
+    if (indata.indexOf("paramde") != -1) //Признак конца передачи (поправил здесь, ибо обрабатываем всё в конце)
     {
-
-        if (controllerData.indexOf("temp2:") == 1) //перед нами часть строки с температурой
+        qDebug()<<"Получили все данные";
+        qDebug()<<controllerData;
+        if (controllerData.indexOf("temp2:") != -1) //перед нами часть строки с температурой, поменял на !=-1 признак того что найдено
         {
-            for(int i = 1; ; i++)
-            {
-                if(controllerData[controllerData.indexOf("temp2:")+i] >= 0 ||
-                   controllerData[controllerData.indexOf("temp2:")+i] <= 9)   //в буффере теперь число из символов
-                {
-                    BufferString = controllerData[controllerData.indexOf("temp2:")+i];
-                }
-                else
-                {
-                    break;
-                }
-            }
+            QString str="temp2:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
             val = BufferString.toInt(&ok, 10);
             ui->panel_out_temp->display(val);
             BufferString.clear();
         }
 
-        if (controllerData.indexOf("humidair:") == 1)
+        if (controllerData.indexOf("humidair:") != -1)
         {
-            for(int i = 1; ; i++)
-            {
-                if(controllerData[controllerData.indexOf("humidair:")+i] >= 0 ||
-                   controllerData[controllerData.indexOf("humidair:")+i] <= 9)   //в буффере теперь число из символов
-                {
-                    BufferString = controllerData[controllerData.indexOf("humidair:")+i];
-                }
-                else
-                {
-                    break;
-                }
-            }
+            QString str="humidair:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1); //Запоминаем значения от начала и конца + 1
             val = BufferString.toInt(&ok, 10);
             ui->panel_out_air->display(val);
             BufferString.clear();
         }
 
-        if (controllerData.indexOf("humidearth:") == 1)
+        if (controllerData.indexOf("humidearth:") != -1)
         {
-            for(int i = 1; ; i++)
-            {
-                if(controllerData[controllerData.indexOf("humidearth:")+i] >= 0 ||
-                   controllerData[controllerData.indexOf("humidearth:")+i] <= 9)   //в буффере теперь число из символов
-                {
-                    BufferString = controllerData[controllerData.indexOf("humidearth:")+i];
-                }
-                else
-                {
-                    break;
-                }
-            }
+            QString str="humidearth:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
             val = BufferString.toInt(&ok, 10);
             ui->panel_out_ground->display(val);
             BufferString.clear();
         }
 
-        if (controllerData.indexOf("gasq:") == 1)
+        if (controllerData.indexOf("gasq:") != -1)
         {
-            for(int i = 1; ; i++)
-            {
-                if(controllerData[controllerData.indexOf("gasq:")+i] >= 0 ||
-                   controllerData[controllerData.indexOf("gasq:")+i] <= 9)   //в буффере теперь число из символов
-                {
-                    BufferString = controllerData[controllerData.indexOf("gasq:")+i];
-                }
-                else
-                {
-                    break;
-                }
-            }
+            QString str="gasq:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
             val = BufferString.toInt(&ok, 10);
             ui->panel_out_co->display(val);
             BufferString.clear();
         }
 
-        if (controllerData.indexOf("psimmhg:") == 1)
-        {
-            for(int i = 1; ; i++)
-            {
-                if(controllerData[controllerData.indexOf("psimmhg:")+i] >= 0 ||
-                   controllerData[controllerData.indexOf("psimmhg:")+i] <= 9)   //в буффере теперь число из символов
-                {
-                    BufferString = controllerData[controllerData.indexOf("psimmhg:")+i];
-                }
-                else
-                {
-                    break;
-                }
-            }
+        if (controllerData.indexOf("psimmhg:") != -1)
+        {QString str="psimmhg:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
             val = BufferString.toInt(&ok, 10);
             ui->panel_out_hg->display(val);
             BufferString.clear();
         }
+        if (controllerData.indexOf("needtemp:") != -1)
+        {QString str="needtemp:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            ui->panel_temp->display(val);
+            BufferString.clear();
+        }
+        if (controllerData.indexOf("needhum:") != -1)
+        {QString str="needhum:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            ui->panel_air->display(val);
+            BufferString.clear();
+        }
+        if (controllerData.indexOf("needhumearth:") != -1)
+        {QString str="needhumearth:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            ui->panel_ground->display(val);
+            BufferString.clear();
+        }
+        if (controllerData.indexOf("maxco2:") != -1)
+        {QString str="maxco2:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            ui->panel_co->display(val);
+            BufferString.clear();
+        }
+        if (controllerData.indexOf("ispumpon:") != -1)
+        {QString str="ispumpon:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            if (val==0){
+            ui->lamp_pump->setBackgroundBrush(Qt::red);
+            ui->label_pump->setText("Насос деактивирован");
+            }
+            if (val==1){
+            ui->lamp_pump->setBackgroundBrush(Qt::green);
+            ui->label_pump->setText("Насос активирован");
+            }
+            BufferString.clear();
+
+        }
+        if (controllerData.indexOf("iswindowopen:") != -1)
+        {QString str="iswindowopen:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            if (val==0){
+            ui->lamp_window->setBackgroundBrush(Qt::red);
+       ui->label_window->setText("Окно закрыто");
+            }
+            if (val==1){
+             ui->lamp_window->setBackgroundBrush(Qt::green);
+                ui->label_window->setText("Окно открыто");
+            }
+            BufferString.clear();
+        }
+        if (controllerData.indexOf("autowork:") != -1)
+        {QString str="autowork:";
+            int start=controllerData.indexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()); //Находим начало цифирь
+            int finish=controllerData.lastIndexOf(QRegExp("[0-9]"),controllerData.indexOf(str)+str.size()+5); //Находим конец цифирь
+            QString BufferString=controllerData.mid(start,finish-start+1);
+            val = BufferString.toInt(&ok, 10);
+            if (val==0)
+            {
+
+            }
+            if (val==1)
+            {
+
+            }
+            BufferString.clear();
+        }
         controllerData.clear();
     }
+}
+
+void MainWindow::showEvent(QShowEvent *ev)
+{
+    QMainWindow::showEvent(ev);
+    showEventHelper();
+}
+void MainWindow::showEventHelper() //События при появлении окна не экране, при отладке заккоментировать, чтоб программа не вылетела
+{
+    connect(timer, SIGNAL(timeout()), this, SLOT(GetData())); //Объединяем таймер и запрос
+    timer->start(250); //Период таймера 2000 мс
+    connect(serialuse,&Serialuse::outData,this,&MainWindow::datain); //Получение данных
+}
+
+void MainWindow::on_setauto_clicked()
+{
+ serialuse->writePort(QByteArray().append("setautowork1"));
+}
+
+
+void MainWindow::on_setmanual_clicked()
+{
+ serialuse->writePort(QByteArray().append("setautowork0"));
 }
