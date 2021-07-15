9db708137154a7c5c497544fbcc63d19220e666c
zuwarka <76390605+zuwarka@users.noreply.github.com>
2021-07-15 14:25:16 +0300

    Update mainwindow.h

--- practise02/Qt/mainwindow.h
+++ practise02/Qt/mainwindow.h
@@ -5,9 +5,11 @@
 #include <QApplication>
 #include <QSlider>
 #include <QString>
+#include <QTimer>
 #include <QLCDNumber>
 #include <QGraphicsScene>
 #include <serialuse.h>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -26,6 +28,9 @@ private:
     QGraphicsScene* scene; //для закраски "ламп"
     Serialuse* serialuse; //соединение с портами
     QString controllerData; //строка данных с МК
+     QTimer *timer; //Таймер для запроса данных
+     void showEventHelper();
+
 
 private slots:
     void on_slider_temp_valueChanged(int value);
@@ -33,14 +38,21 @@ private slots:
     void on_slider_air_valueChanged(int value);
     void on_slider_ground_valueChanged(int value);
     void on_button_on_wifi_clicked();
-    void on_button_off_wifi_clicked();
     void on_button_on_window_clicked();
     void on_button_off_window_clicked();
     void on_button_on_pump_clicked();
     void on_button_off_pump_clicked();
-    void on_slider_temp_sliderReleased();
+   // void on_slider_temp_sliderReleased();
     void GetData(); //Запрос на получение данных
     void datain(QByteArray indata); //Получение данных с МК
+    void on_setauto_clicked();
+
+    void on_setmanual_clicked();
+
+signals:
+    void startwindow();
+protected:
+    void showEvent(QShowEvent *ev);
 };
 
 #endif // MAINWINDOW_H
