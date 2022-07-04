33b885e43e162e9061c4e7f97b7f41e605a38fd0
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-07-04 15:48:25 +0300

    Create Egypt.h

---
+++ practise03/commision01/Egypt.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <map>
+
+class Egypt
+{
+private:
+
+	int n, m, capital;
+
+public:
+
+	void check_way();
+};
