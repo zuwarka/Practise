7a4cb2cbb072345ac0d7cfca8ac0c0f6837fb908
zuwarka <76390605+zuwarka@users.noreply.github.com>
2022-07-04 15:52:00 +0300

    Create Set_Point.h

---
+++ practise03/commision03/Set_Point.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <cmath>
+#include <Windows.h>
+
+class Set_Point
+{
+public:
+    POINT* A, * B, * C;
+    double a, b, c;
+    double perimeter;
+};
